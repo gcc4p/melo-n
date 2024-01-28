72f72a21cf3fc50cacb732ae70bad45a21b7b7d0
Ida_Su <idasuozdemir@gmail.com>
2024-01-28 09:17:53 -0500

    changed scripts.js and added audios

--- static/scripts.js
+++ static/scripts.js
@@ -1,41 +1,76 @@
 // we will have a form that takes input and check if this input is a song name in our sample if it is
 // we show the hidden div and show the mp3 player of this original song and the one generated with AI
-function submitForm(event) {
-    event.preventDefault(); // Prevent the default form submission
+function setSongDetails(genre, song, originalSrc, melonedSrc) {
     
-    // Get the value of the genre select
-    var genre = document.getElementById('GenreSelect').value;
-    
-    // Get the value of the song select
-    var song = document.getElementById('SongSelect').value;
-    if(genre == 1 && song == 2){
-        document.getElementById('')
-    }
-    // Do something with the values, e.g., log to console or send to server
-    console.log("Selected genre: ", genre);
-    console.log("Selected song: ", song);
     var original = document.getElementById('originalSong');
-    var meloned = document.getElementById('melonedsong');
-    original.src ="../static/sounds/audio_example.mp3";
+    var meloned = document.getElementById('MelonedSong');
+    original.src = originalSrc;
+    meloned.src = melonedSrc;
+
     var audioPlayer = document.getElementById('audioPlayer1');
     audioPlayer.load();
-    // For example, to send this data to a server, you might use:
-    // fetch('/submit-form', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({ genre: genre, song: song }),
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //   console.log('Success:', data);
-    // })
-    // .catch((error) => {
-    //   console.error('Error:', error);
-    // });
-  }
-  
-  // Attach the event listener to the form
-  var form = document.getElementById('songForm');
-  form.addEventListener('submit', submitForm);
+    var audioPlayer2 = document.getElementById('audioPlayer2');
+    audioPlayer2.load();
+}
+
+function submitForm(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    var genre = document.getElementById('GenreSelect').value;
+    var song = document.getElementById('SongSelect').value;
+
+    switch (genre) {
+        case '1':
+            switch (song) {
+                case '1':
+                    setSongDetails(genre, song, "../static/sounds/anlamazdin.mp3", "../static/sounds/anlamazdin-rock.wav");
+                    break;
+                case '2':
+                    setSongDetails(genre, song, "../static/sounds/bach.mp3", "../static/sounds/bach-rock.wav");
+                    break;
+                case '3':
+                    setSongDetails(genre, song, "../static/sounds/jojos.mp3", "../static/sounds/jojos-rock.wav");
+                    break;
+                case '4':
+                    setSongDetails(genre, song, "../static/sounds/titanic.mp3", "../static/sounds/titanic-rock.wav");
+                    break;
+            }
+            break;
+        case '2':
+            switch (song) {
+                case '1':
+                    setSongDetails(genre, song, "../static/sounds/anlamazdin.mp3", "../static/sounds/anlamazdin-pop.wav");
+                    break;
+                case '2':
+                    setSongDetails(genre, song, "../static/sounds/bach.mp3", "../static/sounds/bach-pop.wav");
+                    break;
+                case '3':
+                    setSongDetails(genre, song, "../static/sounds/jojos.mp3", "../static/sounds/jojos-pop.wav");
+                    break;
+                case '4':
+                    setSongDetails(genre, song, "../static/sounds/titanic.mp3", "../static/sounds/titanic-pop.wav");
+                    break;
+            }
+            break;
+        case '3':
+            switch (song) {
+                case '1':
+                    setSongDetails(genre, song, "../static/sounds/anlamazdin.mp3", "../static/sounds/anlamazdin-hiphop.wav");
+                    break;
+                case '2':
+                    setSongDetails(genre, song, "../static/sounds/bach.mp3", "../static/sounds/bach-hiphop.wav");
+                    break;
+                case '3':
+                    setSongDetails(genre, song, "../static/sounds/jojos.mp3", "../static/sounds/jojos-hiphop.wav");
+                    break;
+                case '4':
+                    setSongDetails(genre, song, "../static/sounds/titanic.mp3", "../static/sounds/titanic-hiphop.wav");
+                    break;
+            }
+            break;
+    }
+}
+
+// Attach the event listener to the form
+var form = document.getElementById('songForm');
+form.addEventListener('submit', submitForm);
