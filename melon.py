2f6f9aacb7f44888c699b96580eced7cce3731c9
Ida Su Ozdemir <idasuozdemir@gmail.com>
2024-01-27 19:14:46 -0500

    added new html and changes on melon.py

--- melon.py
+++ melon.py
@@ -1,9 +1,13 @@
 from flask import Flask, redirect, url_for, render_template
+import requests
+import json
 
 app = Flask(__name__, static_url_path='/static') 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def trial():
+    req = requests.get() # add api website link here
+    print(req.content)
     return render_template("index.html")
 
 
